1e93bd8cb17c825273b2cdb29e81f7704174aec3
jackoregankenny <42699197+jackoregankenny@users.noreply.github.com>
2021-04-06 21:15:05 +0100

    Initial Upload

---
+++ site.config.js
@@ -0,0 +1,52 @@
+module.exports = {
+    // where it all starts -- the site's root Notion page (required)
+    rootNotionPageId: 'e30660ff57944cef97b5f448aaa47d7f',
+
+    // if you want to restrict pages to a single notion workspace (optional)
+    // (this should be a Notion ID; see the docs for how to extract this)
+    rootNotionSpaceId: null,
+
+    // basic site info (required)
+    name: "Jack O'Regan Kenny",
+    domain: 'jackoregankenny.com',
+    author: "Jack O'Regan Kenny",
+
+    // open graph metadata (optional)
+    description: "Hi I'm Jack and I build things",
+    socialImageTitle: "Jack O'Regan Kenny",
+    socialImageSubtitle: 'I build stuff',
+
+    // social usernames (optional)
+    twitter: 'jackoregankenny',
+    github: 'jackoregankenny',
+    linkedin: 'jackoregankenny',
+    email: 'mailto:jackoregankenny@gmail.com',
+
+    // default notion icon and cover images for site-wide consistency (optional)
+    // page-specific values will override these site-wide defaults
+    defaultPageIcon: null,
+    defaultPageCover: null,
+    defaultPageCoverPosition: 0.5,
+
+    // image CDN host to proxy all image requests through (optional)
+    // NOTE: this requires you to set up an external image proxy
+    imageCDNHost: null,
+
+    // Utteranc.es comments via GitHub issue comments (optional)
+    utterancesGitHubRepo: null,
+
+    // whether or not to enable support for LQIP preview images (optional)
+    // NOTE: this requires you to set up Google Firebase and add the environment
+    // variables specified in .env.example
+    isPreviewImageSupportEnabled: false,
+
+    // map of notion page IDs to URL paths (optional)
+    // any pages defined here will override their default URL paths
+    // example:
+    //
+    // pageUrlOverrides: {
+    //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
+    //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
+    // }
+    pageUrlOverrides: null
+}
